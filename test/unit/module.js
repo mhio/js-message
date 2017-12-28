9fc6c1679dc604166d65eb53a11f20c720e7d758
Matt Hoyle <code@deployable.co>
2017-12-28 04:37:10 +0000

    Make module work

---
+++ test/unit/module.js
@@ -0,0 +1,14 @@
+
+const { Message, SocketMessage } = require('../../')
+
+describe('module', function(){
+
+  it('should load the module', function(){
+    expect( Message ).to.be.ok
+  })
+
+  it('should load the module', function(){
+    expect( SocketMessage ).to.be.ok
+  })
+
+})
